3448360084d502d463923dde94a9d51fd2ec7d48
Github11200 <78944617+Github11200@users.noreply.github.com>
2025-01-29 13:27:43 -0800

    finished half of the functionality

--- ATM/BankAccount.java
+++ ATM/BankAccount.java
@@ -2,186 +2,186 @@ import java.util.*;
 import java.text.*;
 
 /*
- * CERTAIN METHODS AND VARIABLES ARE PUBLIC WHEN THEY SHOULDN'T BE FOR TESTING PURPOSES SUCH AS pswd and all of the generator functions 
-*/
+ * CERTAIN METHODS AND VARIABLES ARE PUBLIC WHEN THEY SHOULDN'T BE FOR TESTING PURPOSES SUCH AS pswd and all of the generator functions
+ */
 
 public class BankAccount {
-  static int MAX_PASSWORD_LENGTH = 5;
+    static int MAX_PASSWORD_LENGTH = 5;
 
-  int acctNum;
-  double balance;
-  String fName;
-  String lName;
-  String pswd;
-  String log;
+    int acctNum;
+    double balance;
+    String fName;
+    String lName;
+    String pswd;
+    String log;
 
-  /***************************** Constructors *****************************/
-  BankAccount() {
-    acctNum = genAcctNum(5);
-    balance = 0;
-    fName = null;
-    lName = null;
-    pswd = genPswd(10);
-    log = "";
-  }
-
-  BankAccount(String firstName, String lastName) {
-    acctNum = genAcctNum(5);
-    balance = 0;
-    fName = firstName;
-    lName = lastName;
-    pswd = genPswd(10);
-    log = "";
-  }
-
-  BankAccount(int accountNumber, double accountBalance, String firstName, String lastName, String password,
-      String defaultLog) {
-    acctNum = accountNumber;
-    balance = accountBalance;
-    fName = firstName;
-    lName = lastName;
-    pswd = password;
-    log = defaultLog;
-  }
-
-  /**********************************************************************/
-
-  /***************************** Transactions *****************************/
-  boolean deposit(double amount) {
-    if (amount >= 0) {
-      balance += amount;
-      log += "\t" + genTimestamp() + "  Deposit Successful [$" + String.format("%.2f", amount) + "]\n";
-
-      return true;
+    /***************************** Constructors *****************************/
+    BankAccount() {
+        acctNum = genAcctNum(5);
+        balance = 0;
+        fName = null;
+        lName = null;
+        pswd = genPswd(10);
+        log = "";
     }
-    return false;
-  }
 
-  boolean withdraw(double amount) {
-    if (amount >= 0 && balance >= amount) {
-      balance -= amount;
-      log += "\t" + genTimestamp() + "  Withdrawal Successful [$" + String.format("%.2f", amount) + "]\n";
-
-      return true;
+    BankAccount(String firstName, String lastName) {
+        acctNum = genAcctNum(5);
+        balance = 0;
+        fName = firstName;
+        lName = lastName;
+        pswd = genPswd(10);
+        log = "";
     }
-    return false;
-  }
 
-  boolean transferTo(double amount, BankAccount targetAccount) {
-    if (amount >= 0 && withdraw(amount)) {
-      targetAccount.deposit(amount);
-      log += "\t" + genTimestamp() + "  Transfer [$" + amount + " to account " + targetAccount.acctNum + "]\n";
-      log += "\t" + genTimestamp() + "  Transfer [$" + amount + " received from account " + acctNum + "]\n";
-      return true;
+    BankAccount(int accountNumber, double accountBalance, String firstName, String lastName, String password,
+                String defaultLog) {
+        acctNum = accountNumber;
+        balance = accountBalance;
+        fName = firstName;
+        lName = lastName;
+        pswd = password;
+        log = defaultLog;
     }
-    log += "\t" + genTimestamp() + "  Transfer Failed [$" + amount + " to account " + targetAccount.acctNum + "]\n";
-    return false;
-  }
 
-  /**********************************************************************/
+    /**********************************************************************/
 
-  /***************************** Setters *****************************/
-  boolean checkPswd(String password) {
-    return password.equals(pswd);
-  }
-
-  boolean resetPswd(String currentPassword, String newPassword) {
-    if (checkPswd(currentPassword)) {
-      pswd = newPassword;
-      log += "\t" + genTimestamp() + "  Password Successfully Changed!\n";
-
-      return true;
+    public static void main(String[] args) {
     }
-    log += "\t" + genTimestamp() + "  Reset Password Failed!\n";
-    return false;
-  }
 
-  void resetAcctNum() {
-    acctNum = genAcctNum(5);
-  }
+    /***************************** Transactions *****************************/
+    boolean deposit(double amount) {
+        if (amount >= 0) {
+            balance += amount;
+            log += "\t" + genTimestamp() + "  Deposit Successful [$" + String.format("%.2f", amount) + "]\n";
 
-  void setFName(String firstName) {
-    fName = firstName;
-  }
-
-  void setLName(String lastName) {
-    lName = lastName;
-  }
-
-  /*****************************************************************/
-
-  /***************************** Getters *****************************/
-  String getFName() {
-    return fName;
-  }
-
-  String getLName() {
-    return lName;
-  }
-
-  String getLog() {
-    return log;
-  }
-
-  double getBalance() {
-    return balance;
-  }
-
-  int getAcctNum() {
-    return acctNum;
-  }
-
-  /*****************************************************************/
-
-  /***************************** Generators *****************************/
-  int genAcctNum(int length) {
-    int min = (int) Math.pow(10, length - 1);
-    int max = (int) Math.pow(10, length);
-    return (int) (min + (Math.random() * (max - min)));
-  }
-
-  String genPswd(int length) {
-    String[] strArray = { "a", "b", "c", "d", "e", "f", "g", "h", "i",
-        "j", "k", "l", "m", "n", "o", "p", "q", "r",
-        "s", "t", "u", "v", "w", "x", "y", "z", "A",
-        "B", "C", "D", "E", "F", "G", "H", "I", "J",
-        "K", "L", "M", "N", "O", "P", "Q", "R", "S",
-        "T", "U", "V", "W", "X", "Y", "Z", "1", "2",
-        "3", "4", "5", "6", "7", "8", "9", "0" };
-
-    if (length > 0) {
-      Random randomNumberGenerator = new Random();
-      String password = "";
-      while (password.length() != length) {
-        password += strArray[randomNumberGenerator.nextInt(26)];
-        if (password.length() == length)
-          break;
-        password += strArray[randomNumberGenerator.nextInt(26) + 26];
-        if (password.length() == length)
-          break;
-        password += strArray[randomNumberGenerator.nextInt(9) + 52];
-      }
-      return password;
+            return true;
+        }
+        return false;
     }
-    return "";
-  }
 
-  String genTimestamp() {
-    String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-    return timeStamp;
-  }
+    boolean withdraw(double amount) {
+        if (amount >= 0 && balance >= amount) {
+            balance -= amount;
+            log += "\t" + genTimestamp() + "  Withdrawal Successful [$" + String.format("%.2f", amount) + "]\n";
 
-  /********************************************************************/
+            return true;
+        }
+        return false;
+    }
 
-  void display() {
-    System.out.println("Account Number: " + acctNum);
-    System.out.println("Balance: " + balance);
-    System.out.println("First name: " + fName);
-    System.out.println("Last name: " + lName);
-    System.out.println("Password: " + pswd);
-    System.out.println("LOG:\n" + log);
-  }
+    /**********************************************************************/
 
-  public static void main(String[] args) {
-  }
+    boolean transferTo(double amount, BankAccount targetAccount) {
+        if (amount >= 0 && withdraw(amount)) {
+            targetAccount.deposit(amount);
+            log += "\t" + genTimestamp() + "  Transfer [$" + amount + " to account " + targetAccount.acctNum + "]\n";
+            targetAccount.log += "\t" + genTimestamp() + "  Transfer [$" + amount + " received from account " + acctNum + "]\n";
+            return true;
+        }
+        log += "\t" + genTimestamp() + "  Transfer Failed [$" + amount + " to account " + targetAccount.acctNum + "]\n";
+        return false;
+    }
+
+    /***************************** Setters *****************************/
+    boolean checkPswd(String password) {
+        return password.equals(pswd);
+    }
+
+    boolean resetPswd(String currentPassword, String newPassword) {
+        if (checkPswd(currentPassword)) {
+            pswd = newPassword;
+            log += "\t" + genTimestamp() + "  Password Successfully Changed!\n";
+
+            return true;
+        }
+        log += "\t" + genTimestamp() + "  Reset Password Failed!\n";
+        return false;
+    }
+
+    void resetAcctNum() {
+        acctNum = genAcctNum(5);
+    }
+
+    /***************************** Getters *****************************/
+    String getFName() {
+        return fName;
+    }
+
+    /*****************************************************************/
+
+    void setFName(String firstName) {
+        fName = firstName;
+    }
+
+    String getLName() {
+        return lName;
+    }
+
+    void setLName(String lastName) {
+        lName = lastName;
+    }
+
+    String getLog() {
+        return log;
+    }
+
+    double getBalance() {
+        return balance;
+    }
+
+    /*****************************************************************/
+
+    int getAcctNum() {
+        return acctNum;
+    }
+
+    /***************************** Generators *****************************/
+    int genAcctNum(int length) {
+        int min = (int) Math.pow(10, length - 1);
+        int max = (int) Math.pow(10, length);
+        return (int) (min + (Math.random() * (max - min)));
+    }
+
+    String genPswd(int length) {
+        String[] strArray = {"a", "b", "c", "d", "e", "f", "g", "h", "i",
+                "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                "s", "t", "u", "v", "w", "x", "y", "z", "A",
+                "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                "T", "U", "V", "W", "X", "Y", "Z", "1", "2",
+                "3", "4", "5", "6", "7", "8", "9", "0"};
+
+        if (length > 0) {
+            Random randomNumberGenerator = new Random();
+            String password = "";
+            while (password.length() != length) {
+                password += strArray[randomNumberGenerator.nextInt(26)];
+                if (password.length() == length)
+                    break;
+                password += strArray[randomNumberGenerator.nextInt(26) + 26];
+                if (password.length() == length)
+                    break;
+                password += strArray[randomNumberGenerator.nextInt(9) + 52];
+            }
+            return password;
+        }
+        return "";
+    }
+
+    String genTimestamp() {
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        return timeStamp;
+    }
+
+    /********************************************************************/
+
+    void display() {
+        System.out.println("Account Number: " + acctNum);
+        System.out.println("Balance: " + balance);
+        System.out.println("First name: " + fName);
+        System.out.println("Last name: " + lName);
+        System.out.println("Password: " + pswd);
+        System.out.println("LOG:\n" + log);
+    }
 }
--- ATM/BankAccountManagerGUI.java
+++ ATM/BankAccountManagerGUI.java
@@ -3,14 +3,30 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/*******************************************
+ * WORK ON THE CHANGE PASSWORD 🙏🙏🙏🙏
+ ******************************************/
+
 public class BankAccountManagerGUI extends JFrame {
+    static BankAccountManager bankAccountManager = new BankAccountManager();
+    final private CardLayout cardLayout = new CardLayout();
+    final private String ROOT_PANEL_ID = "RootPanel";
+    final private String LOGIN_PANEL_ID = "LoginPanel";
+    final private String REGISTER_PANEL_ID = "RegisterPanel";
+    final private String MAIN_PANEL_ID = "MainPanel";
+    final private String DEPOSIT_PANEL_ID = "DepositPanel";
+    final private String WITHDRAW_PANEL_ID = "WithdrawPanel";
+    final private String TRANSFER_PANEL_ID = "TransferPanel";
+    final private String ACCOUNT_INFO_PANEL_ID = "AccountInfoPanel";
+    final private String TRANSACTION_PANEL_ID = "TransactionPanel";
+    final private String CHANGE_PASSWORD_PANEL_ID = "ChangePasswordPanel";
+    BankAccount currentBankAccount = null;
     private JButton LOGINButton;
     private JPanel panel;
     private JPanel loginPanel;
     private JPanel mainPanel;
     private JButton loginButton;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
+    private JPasswordField passwordTextField;
     private JButton rootDepositButton;
     private JButton rootWithdrawButton;
     private JButton rootTransferToButton;
@@ -33,9 +49,8 @@ public class BankAccountManagerGUI extends JFrame {
     private JButton withdrawButton;
     private JLabel accountNumber;
     private JLabel accountBalance;
-    private JLabel firstName;
-    private JLabel password;
-    private JLabel lastName;
+    private JLabel accountFirstName;
+    private JLabel accountLastName;
     private JLabel transactions;
     private JPasswordField passwordField2;
     private JPasswordField passwordField3;
@@ -63,21 +78,9 @@ public class BankAccountManagerGUI extends JFrame {
     private JCheckBox generatePasswordCheckBox;
     private JButton createAccountButton;
     private JButton registerExitButton;
-    final private CardLayout cardLayout = new CardLayout();
-
-    final private String ROOT_PANEL_ID = "RootPanel";
-    final private String LOGIN_PANEL_ID = "LoginPanel";
-    final private String REGISTER_PANEL_ID = "RegisterPanel";
-    final private String MAIN_PANEL_ID = "MainPanel";
-    final private String DEPOSIT_PANEL_ID = "DepositPanel";
-    final private String WITHDRAW_PANEL_ID = "WithdrawPanel";
-    final private String TRANSFER_PANEL_ID = "TransferPanel";
-    final private String ACCOUNT_INFO_PANEL_ID = "AccountInfoPanel";
-    final private String TRANSACTION_PANEL_ID = "TransactionPanel";
-    final private String CHANGE_PASSWORD_PANEL_ID = "ChangePasswordPanel";
-
-    static BankAccountManager bankAccountManager = new BankAccountManager();
-    BankAccount currentBankAccount = null;
+    private JTextField usernameTextField;
+    private JScrollPane transactionsScrollPanel;
+    private JLabel transactionsText;
 
     public BankAccountManagerGUI() {
         try {
@@ -110,7 +113,8 @@ public class BankAccountManagerGUI extends JFrame {
         cardLayout.show(panel, ROOT_PANEL_ID);
         =================================================*/
 
-        cardLayout.show(panel, ROOT_PANEL_ID);
+        cardLayout.show(panel, MAIN_PANEL_ID);
+        currentBankAccount = new BankAccount("Luke", "Skywalker");
 
         setTitle("Bank Account Manager");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -128,11 +132,34 @@ public class BankAccountManagerGUI extends JFrame {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                cardLayout.show(panel, MAIN_PANEL_ID);
+                try {
+                    int accountNumber = Integer.parseInt(usernameTextField.getText());
+                    currentBankAccount = bankAccountManager.getAccount(accountNumber);
+                    if (currentBankAccount == null)
+                        throw new IllegalArgumentException("Invalid account number");
+                    else if (!currentBankAccount.checkPswd(String.valueOf(passwordTextField.getPassword()))) {
+                        currentBankAccount = null;
+                        throw new IllegalArgumentException("Invalid password");
+                    }
+                    cardLayout.show(panel, MAIN_PANEL_ID);
+                } catch (Exception e1) {
+                    String message = e1.getMessage();
+                    if (e1 instanceof NumberFormatException)
+                        message = "Please enter your account number, not any other characters";
+                    JDialog errorDialog = new JDialog();
+                    JLabel errorLabel = new JLabel(message, JLabel.CENTER);
+
+                    errorDialog.setTitle("Error");
+                    errorDialog.setModal(true);
+                    errorDialog.setLocationRelativeTo(null);
+                    errorDialog.setSize(300, 200);
+                    errorDialog.add(errorLabel, JLabel.CENTER);
+                    errorDialog.setVisible(true);
+                    usernameTextField.setText("");
+                    passwordTextField.setText("");
+                }
             }
         });
-
-
 
         rootDepositButton.addActionListener(new ActionListener() {
             @Override
@@ -159,6 +186,10 @@ public class BankAccountManagerGUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 cardLayout.show(panel, ACCOUNT_INFO_PANEL_ID);
+                accountNumber.setText("Account Number: " + currentBankAccount.getAcctNum());
+                accountBalance.setText("Balance: $" + String.format("%.2f", currentBankAccount.getBalance()));
+                accountFirstName.setText("First Name: " + currentBankAccount.getFName());
+                accountLastName.setText("Last Name: " + currentBankAccount.getLName());
             }
         });
 
@@ -166,6 +197,7 @@ public class BankAccountManagerGUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 cardLayout.show(panel, TRANSACTION_PANEL_ID);
+                transactionsText.setText(currentBankAccount.getLog());
             }
         });
 
@@ -179,6 +211,7 @@ public class BankAccountManagerGUI extends JFrame {
         logoutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                currentBankAccount = null;
                 cardLayout.show(panel, ROOT_PANEL_ID);
             }
         });
@@ -238,6 +271,54 @@ public class BankAccountManagerGUI extends JFrame {
                 cardLayout.show(panel, REGISTER_PANEL_ID);
             }
         });
+
+        depositButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int amount = (Integer) depositAmount.getValue();
+                boolean deposited = currentBankAccount.deposit(amount);
+                String message = "";
+                if (deposited) message = "Deposited successfully!";
+                else message = "Deposit failed!";
+                JDialog errorDialog = new JDialog();
+                JLabel errorLabel = new JLabel(message, JLabel.CENTER);
+
+                errorDialog.setModal(true);
+                errorDialog.setLocationRelativeTo(null);
+                errorDialog.setSize(300, 200);
+                errorDialog.add(errorLabel, JLabel.CENTER);
+                errorDialog.setVisible(true);
+                depositAmount.setValue(0);
+            }
+        });
+
+        withdrawButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int amount = (Integer) withdrawAmount.getValue();
+                boolean withdrawn = currentBankAccount.withdraw(amount);
+                String message = "";
+                if (withdrawn) message = "Withdrawn successfully!";
+                else message = "Withdraw failed! Not enough account balance.";
+
+                JDialog errorDialog = new JDialog();
+                JLabel errorLabel = new JLabel(message, JLabel.CENTER);
+
+                errorDialog.setModal(true);
+                errorDialog.setLocationRelativeTo(null);
+                errorDialog.setSize(300, 200);
+                errorDialog.add(errorLabel, JLabel.CENTER);
+                errorDialog.setVisible(true);
+                depositAmount.setValue(0);
+            }
+        });
+
+        changePasswordButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -250,6 +331,7 @@ public class BankAccountManagerGUI extends JFrame {
         bankAccountManager.addAcct(bankAccount3);
         bankAccountManager.display();
 
+        bankAccount1.display();
         new BankAccountManagerGUI();
     }
 }
